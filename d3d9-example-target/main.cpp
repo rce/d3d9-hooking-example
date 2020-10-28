252471985eab72869287291cb18c69187b0937c2
Henry Heikkinen <rce@rce.fi>
2020-10-28 18:56:21 +0200

    Implement a simple D3D9 render loop

--- d3d9-example-target/main.cpp
+++ d3d9-example-target/main.cpp
@@ -1,6 +1,37 @@
 #include <Windows.h>
+#include <d3d9.h>
+
+#pragma comment (lib, "d3d9.lib")
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+LPDIRECT3D9 g_pD3D;
+LPDIRECT3DDEVICE9 g_pDevice;
+
+void initD3D(HWND hWnd)
+{
+	g_pD3D = Direct3DCreate9(D3D_SDK_VERSION);
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.hDeviceWindow = hWnd;
+	g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pDevice);
+}
+
+void render()
+{
+	g_pDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
+	g_pDevice->BeginScene();
+	g_pDevice->EndScene();
+	g_pDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+void cleanupD3D()
+{
+	g_pDevice->Release();
+	g_pD3D->Release();
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -18,6 +49,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	HWND hWnd = CreateWindowEx(NULL, wc.lpszClassName, L"d3d9-example-target", WS_OVERLAPPEDWINDOW, 300, 300, 500, 400, NULL, NULL, hInstance, NULL);
 	ShowWindow(hWnd, nCmdShow);
 
+	initD3D(hWnd);
+
 	MSG msg;
 	while (true)
 	{
@@ -28,7 +61,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		}
 
 		if (msg.message == WM_QUIT) break;
+
+		render();
 	}
+
+	cleanupD3D();
 	return msg.wParam;
 }
 
